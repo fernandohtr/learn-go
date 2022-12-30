5a81f8fbf61a49b5cd31f64f992bcf7b0f1abbf2
Fernando Toledo <fernandohtr@gmail.com>
2022-12-30 13:51:04 -0300

    Add new behavior when hello function receives empty string
    - Refactor the test by creating the assertCorrectMessage

--- go-fundamentals/hello-world/hello_test.go
+++ go-fundamentals/hello-world/hello_test.go
@@ -3,11 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	have := Hello("Fernando")
-	// have := Hello()
-	want := "Hello, Fernando"
-	// want := "Hello, world"
+	t.Run("saying hello to people", func(t *testing.T) {
+		have := Hello("Fernando")
+		want := "Hello, Fernando"
+		assertCorrectMessage(t, have, want)
+	})
 
+	t.Run("say 'Hello, World' when an ampty string is supplied", func(t *testing.T) {
+		have := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, have, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, have, want string) {
+	t.Helper()
 	if have != want {
 		t.Errorf("\nhave %q\nwant %q", have, want)
 	}
