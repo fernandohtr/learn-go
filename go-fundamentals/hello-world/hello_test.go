e5fb86b192dd16a69d31dbed3844a7c9932b59fe
Fernando Toledo <fernandohtr@gmail.com>
2022-12-30 13:29:13 -0300

    Change the Hello function to take argument

--- go-fundamentals/hello-world/hello_test.go
+++ go-fundamentals/hello-world/hello_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	received := Hello()
-	expected := "Hello, world"
+	have := Hello("Fernando")
+	// have := Hello()
+	want := "Hello, Fernando"
+	// want := "Hello, world"
 
-	if received != expected {
-		t.Errorf("\nreceived: %q\nexpected: %q", received, expected)
+	if have != want {
+		t.Errorf("\nhave %q\nwant %q", have, want)
 	}
 }
