453f09ae9cb60ddd30e43c7cba31768e2fdb7a64
Fernando Toledo <fernandohtr@gmail.com>
2023-02-13 21:02:26 -0300

    Refactor arrays/sum to receive collection of any size

--- go-fundamentals/arrays/arrays_test.go
+++ go-fundamentals/arrays/arrays_test.go
@@ -3,11 +3,19 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	received := Sum(numbers)
-	expected := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
+		received := Sum(numbers)
+		expected := 6
+
+		assertCorrectMessage(t, received, expected, numbers)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, received int, expected int, numbers []int) {
+	t.Helper()
 	if received != expected {
 		t.Errorf("\nreceived: %d\nexpected: %d\ngiven numbers: %v", received, expected, numbers)
 	}
