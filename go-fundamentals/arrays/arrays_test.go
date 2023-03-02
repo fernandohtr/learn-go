f200b0e98542e6fd75f6551ced377bc8068592ae
Fernando Toledo <fernandohtr@gmail.com>
2023-03-01 21:47:43 -0300

    Add SumAllTails in arrays/sum

--- go-fundamentals/arrays/arrays_test.go
+++ go-fundamentals/arrays/arrays_test.go
@@ -7,18 +7,22 @@ import (
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	numbers := []int{1, 2, 3}
 
-		received := Sum(numbers)
-		expected := 6
+	received := Sum(numbers)
+	expected := 6
 
-		assertCorrectMessage(t, received, expected, numbers)
-	})
+	assertCorrectMessage(t, received, expected, numbers)
+}
+
+func assertCorrectMessage(t testing.TB, received int, expected int, numbers []int) {
+	t.Helper()
+	if received != expected {
+		t.Errorf("\nreceived: %d\nexpected: %d\ngiven numbers: %v", received, expected, numbers)
+	}
 }
 
 func TestSumAll(t *testing.T) {
-	
 	received := SumAll([]int{1, 2}, []int{0, 9})
 	expected := []int{3, 9}
 
@@ -27,9 +31,26 @@ func TestSumAll(t *testing.T) {
 	}
 }
 
-func assertCorrectMessage(t testing.TB, received int, expected int, numbers []int) {
-	t.Helper()
-	if received != expected {
-		t.Errorf("\nreceived: %d\nexpected: %d\ngiven numbers: %v", received, expected, numbers)
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, received, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(received, expected) {
+			t.Errorf("\nreceived: %v\nexpected: %v", received, expected)
+		}
 	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		received := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		checkSums(t, received, expected)
+	})
+
+	t.Run("safely sum empty slices", func (t *testing.T) {
+		received := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+
+		checkSums(t, received, expected)
+	})
 }
