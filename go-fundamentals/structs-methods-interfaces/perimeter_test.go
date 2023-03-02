03184f00cb48319dc6f41f3ca4f63b30802ddd4f
Fernando Toledo <fernandohtr@gmail.com>
2023-03-01 23:31:46 -0300

    Add Triangle type to structsmethosinterfaces package

--- go-fundamentals/structs-methods-interfaces/perimeter_test.go
+++ go-fundamentals/structs-methods-interfaces/perimeter_test.go
@@ -30,4 +30,22 @@ func TestArea(t *testing.T) {
 		circle := Circle{10}
 		checkArea(t, circle, 314.1592653589793)
 	})
+
+	t.Run("table driven tests", func(t *testing.T) {
+		areaTests := []struct {
+			shape    Shape
+			expected float64
+		}{
+			{Rectangle{12, 6}, 72.0},
+			{Circle{10}, 314.1592653589793},
+			{Triangle{12, 6}, 36.0},
+		}
+
+		for _, tt := range areaTests {
+			received := tt.shape.Area()
+			if received != tt.expected {
+				t.Errorf("\nreceived: %g\nexpected: %g", received, tt.expected)
+			}
+		}
+	})
 }
