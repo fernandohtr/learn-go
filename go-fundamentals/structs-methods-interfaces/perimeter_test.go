c6d0034ebd8e99bcf450a2662451fb90427c4916
Fernando Toledo <fernandohtr@gmail.com>
2023-03-01 22:32:31 -0300

    Add Area function to structsmethosinterfaces package

--- go-fundamentals/structs-methods-interfaces/perimeter_test.go
+++ go-fundamentals/structs-methods-interfaces/perimeter_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("\nreceived: %.2f\nexpected: %.2f", received, expected)
 	}
 }
+
+func TestAr(t *testing.T) {
+	received := Area(12.0, 6.0)
+	expected := 72.0
+
+	if received != expected {
+		t.Errorf("\nreceived: %.2f\nexpected: %.2f", received, expected)
+	}
+}
